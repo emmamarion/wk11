bd5ea20ba3a4867a32a2ef6be049473037a71068
Emma Marion <emarion@umich.edu>
2025-11-05 10:18:52 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,17 +1,24 @@
 // Emma Marion
-// When page loads slider should be 0 and go to 100 on video play
-let video = document.querySelector("#player1")
+
+let video = document.querySelector("#player1") // video element
+let slider = document.querySelector("#slider") // slider element
+let volumeSpan = document.querySelector("#volume") // Get the span element that houses the current volume level
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
 	video.loop = false;
-	video.autplay = false;
+	video.autoplay = false;
+	slider.value = 0;
+	volumeSpan.innerHTML = slider.value + "%"
+
 });
 
 // Play button
 document.querySelector("#play").addEventListener("click", function(){
 	console.log("Play video");
 	video.play();
+	slider.value = 100;
+	volumeSpan.innerHTML = slider.value + "%"
 });
 
 // Pause button
@@ -46,18 +53,34 @@ document.querySelector("#mute").addEventListener("click", function(){
 	// If video is muted
 	if (video.muted){
 		video.muted = false;
-		this.innerHTML = "Muted"
+		this.innerHTML = "Mute";
 	}else{
 		video.muted = true;
-		this.innerHTML = "Unmute"
-	}
+		this.innerHTML = "Unmute";
+	};
 });
 
+// Volume slider
 document.querySelector("#slider").addEventListener("change", function(){
-	const sliderValue = this.value;
-	console.log("New slider value is: " + sliderValue)
-	video.volume = sliderValue
+	let sliderValue = this.value; // Get the value of the volume slider
+
+	console.log("New slider value is: " + sliderValue);
+	video.volume = sliderValue * 0.01; // update video volume
+	volumeSpan.innerHTML = sliderValue + "%" // update volume display text 
+
 });
+
+// Vintage buttonx
+document.querySelector("#vintage").addEventListener("click", function(){
+	console.log("Going old school!")
+	video.classList.add("oldSchool")
+})
+
+// Original button
+document.querySelector("#orig").addEventListener("click", function(){
+	console.log("Back to normal!")
+	video.classList.remove("oldSchool")
+})
 
 function play(){
 	console.log("In play")
@@ -66,4 +89,3 @@ function play(){
 function pause(){
 	console.log("In pause")
 }
-
